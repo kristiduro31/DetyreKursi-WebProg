c2d0d6d1fa80b5712db74cfdb7664b45a3ee7187
kristiduro <kristi.duro@fti.edu.al>
2022-11-25 12:58:26 +0100

    Adding services pages and linking them to the navbar references.

--- scripts/components.js
+++ scripts/components.js
@@ -8,13 +8,12 @@ class Navbar extends HTMLElement{
                     <li>
                        <a href="#">Services <i class="fa fa-cogs" style="font-size:100%"></i></a>
                        <ul>
-                          <li><a href="#">Transport <i class="fa fa-taxi" style="margin-left: 0"></i></a></li>
-                          <li><a href="#">Restaurant <i class="fa-solid fa-utensils" style="margin-left: 0; font-size:100%"></i></a></li>
-                          <li><a href="#">Customs <i class="fa-solid fa-person-military-to-person" style="margin-left: 0"></i></a></li>
-                          <li><a href="#">Shops <i class="fa-solid fa-cart-shopping" style="margin-left: 0"></i></a></li>
-                          <li><a href="#">Health <i class="fa fa-medkit" style="margin-left: 0"></i></a></li>
-                          <li><a href="#">Shops <i class="fa fa-shopping-bag" style="margin-left: 0"></i></a></li>
-                          <li><a href="#">Luggage <i class="fa-solid fa-suitcase-rolling" style="margin-left: 0"></i></a></li>
+                          <li><a href="../templates/transport.html">Transport <i class="fa fa-taxi" style="margin-left: 0"></i></a></li>
+                          <li><a href="../templates/restaurant.html">Restaurant <i class="fa-solid fa-utensils" style="margin-left: 0; font-size:100%"></i></a></li>
+                          <li><a href="../templates/customs.html">Customs <i class="fa-solid fa-person-military-to-person" style="margin-left: 0"></i></a></li>
+                          <li><a href="../templates/shops.html">Shops <i class="fa-solid fa-cart-shopping" style="margin-left: 0"></i></a></li>
+                          <li><a href="../templates/health.html">Health <i class="fa fa-medkit" style="margin-left: 0"></i></a></li>
+                          <li><a href="../templates/luggage.html">Luggage <i class="fa-solid fa-suitcase-rolling" style="margin-left: 0"></i></a></li>
                        </ul>
                     </li>
                     <li><a href="#">Car Rentals <i class="fa fa-automobile" style="font-size:100%;"></i></a></li>
@@ -22,7 +21,7 @@ class Navbar extends HTMLElement{
 <!-- afishimi ores --> <li id="clk" style="float: right; color: white; text-align: center; margin-right: 20px; margin-top:23px; font-size: 20px;"><a href="#"></a></li> 
                     <li style="float: right;margin-right: 55px;"><a href="#"><i class="fa fa-user-circle" style="font-size:150%; margin-top: 10px; margin-left: 40px"></i></a>
                         <ul>
-                            <li><a href="login.php">Login</a></li>
+                            <li><a href="login.php" target="_self">Login</a></li>
                         </ul>
                     </li>           
                  </ul>
