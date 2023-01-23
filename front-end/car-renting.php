d39d12aeaf63a0aacfe8583c0792865c1ebf936e
Kevin Halili <kevin.halili@ids.al>
2023-01-23 22:29:14 +0100

    a bit better

--- front-end/car-renting.php
+++ front-end/car-renting.php
@@ -12,132 +12,132 @@
 <body id="car-rental-body" onload="realtimeClock(), getRouting()">
 <?php include "../components/navbar.php" ?>
 <main>
-    <h1>Makina me qera</h1>
-    <br>
-    <br>
-    <div class="rental">
-        <ul class="display-rentals">
-            <li style="display: block;">
-                <details>
-                    <summary><h3>Albanian Airport Rent a Car</h3></summary>
-                    <div class="row">
-                        <div class="column">
-                            <img class="company-img-in" src="../images/car-rentals/alAirRental.png"
-                                 alt="Image could not be displayed">
-                        </div>
-                        <div class="column">
+    <div class="reg-container-main">
+        <h1>Makina me qera</h1>
+        <br>
+        <br>
+        <div class="rental">
+            <ul class="display-rentals">
+                <li style="display: block;">
+                    <details>
+                        <summary><h3>Albanian Airport Rent a Car</h3></summary>
+                        <div class="row">
                             <div class="column">
-                                <h3 style="font-weight: bolder">Përshkrimi</h3>
-                                <p>Ne jemi nje kompani e fokusuar ne dhenien e makinave me qera, sherbim taxi, makina me
-                                    qera me shofer.
+                                <div class="column-inside">
+                                    <img class="company-img-in" src="../images/car-rentals/alAirRental.png"
+                                         alt="Image could not be displayed">
+                                </div>
+                                <div class="column-inside">
+                                    <h3 style="font-weight: bolder">Përshkrimi</h3>
+                                    <p>Ne jemi nje kompani e fokusuar ne dhenien e makinave me qera, sherbim taxi, makina me
+                                        qera me shofer.
 
-                                    Albania Airport Rent a Car u krijua nga Mr. Dashamir Kullaj ne vitin 2003, qe ne
-                                    vitet e para flota e makinave tona perbehej nga markat e automjeteve me prestigjoze
-                                    si Opel, Volkswagen, Mercedes Benz and Ford.</p>
+                                        Albania Airport Rent a Car u krijua nga Mr. Dashamir Kullaj ne vitin 2003, qe ne
+                                        vitet e para flota e makinave tona perbehej nga markat e automjeteve me prestigjoze
+                                        si Opel, Volkswagen, Mercedes Benz and Ford.</p>
+                                </div>
+                                <div class="column-inside">
+                                    <h3>Kontakt</h3>
+                                    <div class="detail">
+                                        <label>Numri i telefonit</label>
+                                        <h4><a href="tel:+355684011010">+355 68 40 11 010</a></h4>
+                                        <h4><a href="tel:+355684088008">+355 68 40 88 008</a></h4>
+                                    </div>
+                                    <div class="detail">
+                                        <label>Email</label>
+                                        <h4>
+                                            <a href="mailto:info@albaniaairportrentacar.al">info@albaniaairportrentacar.al</a>
+                                        </h4>
+                                    </div>
+                                    <div class="detail">
+                                        <label>Website</label>
+                                        <h4><a target="_blank"
+                                               href="http://www.albaniarentalcars.com">www.albaniarentalcars.com</a>
+                                    </div>
+                                </div>
                             </div>
+                    </details>
+                </li>
+                <li style="display: block;">
+                    <details class="car-rental">
+                        <summary><h3>Albania Car Rentals</h3></summary>
+                        <div class="row">
                             <div class="column">
-                                <h3>Kontakt</h3>
-                                <div class="detail">
-                                    <label>Numri i telefonit</label>
-                                    <h4><a href="tel:+355684011010">+355 68 40 11 010</a></h4>
-                                    <h4><a href="tel:+355684088008">+355 68 40 88 008</a></h4>
+                                <div class="column-inside">
+                                    <img class="company-img-in" src="../images/car-rentals/e-rental.png"
+                                         alt="Image could not be displayed">
                                 </div>
-                                <div class="detail">
-                                    <label>Email</label>
-                                    <h4>
-                                        <a href="mailto:info@albaniaairportrentacar.al">info@albaniaairportrentacar.al</a>
-                                    </h4>
+                                <div class="column-inside">
+                                    <h3 style="font-weight: bolder">Përshkrimi</h3>
+                                    <p>Makina me qira me çmime të mira mund t'i gjeni vetëm tek
+                                    Albanian Car Rents. Markat më të suksesshme, cilësi dhe  </p>
                                 </div>
-                                <div class="detail">
-                                    <label>Website</label>
-                                    <h4><a target="_blank"
-                                           href="http://www.albaniarentalcars.com">www.albaniarentalcars.com</a>
+                                <div class="column-inside">
+                                    <h3>Kontakt</h3>
+                                    <div class="detail">
+                                        <label>Numri i telefonit</label>
+                                        <h4><a href="+355676018857">+355 676018857</a></h4>
+                                    </div>
+                                    <div class="detail">
+                                        <label>Email</label>
+                                        <h4><a href="mailto:info@albaniacarrentals.com">info@albaniacarrentals.com</a></h4>
+                                    </div>
+                                    <div class="detail">
+                                        <label>Website</label>
+                                        <h4><a target="_blank"
+                                               href="www.albaniacarrentals.com">www.albaniacarrentals.com</a>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                </details>
-            </li>
-            <li style="display: block;">
-                <details>
-                    <summary><h3>Albania Car Rentals</h3></summary>
-                    <div class="row">
-                        <div class="column">
-                            <div class="image">
-                                <img class="company-img-in" src="../images/car-rentals/e-rental.png"
-                                     alt="Image could not be displayed">
-                            </div>
-                        </div>
 
-                        <div class="column">
-                            <div class="column">
-                                <h3 style="font-weight: bolder">Përshkrimi</h3>
-                                <p>Makina me qira me çmime të mira</p>
                             </div>
+                    </details>
+                </li>
+                <li>
+                    <details>
+                        <summary><h3>Eurocar Rentals</h3></summary>
+                        <div class="row">
                             <div class="column">
-                                <h3>Kontakt</h3>
-                                <div class="detail">
-                                    <label>Numri i telefonit</label>
-                                    <h4><a href="+355676018857">+355 676018857</a></h4>
+                                <div class="column-inside">
+                                    <img class="company-img-in" src="../images/car-rentals/euro.png"
+                                         alt="Image could not be displayed">
                                 </div>
-                                <div class="detail">
-                                    <label>Email</label>
-                                    <h4><a href="mailto:info@albaniacarrentals.com">info@albaniacarrentals.com</a></h4>
-                                </div>
-                                <div class="detail">
-                                    <label>Website</label>
-                                    <h4><a target="_blank"
-                                           href="www.albaniacarrentals.com">www.albaniacarrentals.com</a>
-                                </div>
-                            </div>
+                                <div class="column-inside">
+                                    <h3 style="font-weight: bolder">Përshkrimi</h3>
+                                    <p>Eurocar Rentals është suksesi i radhës i Mektrin Motors në tregun e makinave me qira.
+                                        Parimet tona bazohen në vlera që na ndihmojnë të ndërtojmë marrëdhënie afatgjata me
+                                        klientët, komunitetin, partnerët dhe, më e rëndësishmja, me njëri-tjetrin.
 
-                        </div>
-                </details>
-            </li>
-            <li>
-                <details>
-                    <summary><h3>Eurocar Rentals</h3></summary>
-                    <div class="row">
-                        <div class="column">
-                            <img class="company-img-in" src="../images/car-rentals/euro.png"
-                                 alt="Image could not be displayed">
-                        </div>
-                        <div class="column">
-                            <div class="column">
-                                <h3 style="font-weight: bolder">Përshkrimi</h3>
-                                <p>Eurocar Rentals është suksesi i radhës i Mektrin Motors në tregun e makinave me qira.
-                                    Parimet tona bazohen në vlera që na ndihmojnë të ndërtojmë marrëdhënie afatgjata me
-                                    klientët, komunitetin, partnerët dhe, më e rëndësishmja, me njëri-tjetrin.
+                                        Eurocar ofron plane të personalizuara të makinave me qira dhe një gamë të plotë
+                                        zgjidhjesh transporti, duke ofruar shërbimin më të mirë në treg pasi ne besojmë se
+                                        klientët tanë janë thelbi i kompanisë.
 
-                                    Eurocar ofron plane të personalizuara të makinave me qira dhe një gamë të plotë
-                                    zgjidhjesh transporti, duke ofruar shërbimin më të mirë në treg pasi ne besojmë se
-                                    klientët tanë janë thelbi i kompanisë.
-
-                                    Eurocar do të jetë shoqëruesi juaj më i sigurt në çdo udhëtim, duke qenë sa më afër
-                                    kërkesave dhe pritshmërive tuaja, me shërbim profesional gjatë gjithë procesit dhe
-                                    një shumëllojshmëri të automjeteve dhe shërbimeve tona</p>
-                            </div>
-                            <div class="column">
-                                <h3>Kontakti</h3>
-                                <div class="detail">
-                                    <label>Numri i telefonit</label>
-                                    <h4><a href="tel:+355 4 2255 399">+355 4 2255 399</a></h4>
-                                    <h4><a href="tel:+355 376005454">+355 376005454</a></h4>
+                                        Eurocar do të jetë shoqëruesi juaj më i sigurt në çdo udhëtim, duke qenë sa më afër
+                                        kërkesave dhe pritshmërive tuaja, me shërbim profesional gjatë gjithë procesit dhe
+                                        një shumëllojshmëri të automjeteve dhe shërbimeve tona</p>
                                 </div>
-                                <div class="detail">
-                                    <label>Email</label>
-                                    <h4><a href="mailto:info@albaniaairportrentacar.al">info@eurocar.rentals</a>
-                                    </h4>
-                                </div>
-                                <div class="detail">
-                                    <label>Website</label>
-                                    <h4><a target="_blank"
-                                           href="http://www.albaniarentalcars.com">www.eurocar.al</a>
+                                <div class="column-inside">
+                                    <h3>Kontakti</h3>
+                                    <div class="detail">
+                                        <label>Numri i telefonit</label>
+                                        <h4><a href="tel:+355 4 2255 399">+355 4 2255 399</a></h4>
+                                        <h4><a href="tel:+355 376005454">+355 376005454</a></h4>
+                                    </div>
+                                    <div class="detail">
+                                        <label>Email</label>
+                                        <h4><a href="mailto:info@albaniaairportrentacar.al">info@eurocar.rentals</a>
+                                        </h4>
+                                    </div>
+                                    <div class="detail">
+                                        <label>Website</label>
+                                        <h4><a target="_blank"
+                                               href="http://www.albaniarentalcars.com">www.eurocar.al</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                </details>
-            </li>
-        </ul>
+                    </details>
+                </li>
+            </ul>
+        </div>
     </div>
 </main>
 
