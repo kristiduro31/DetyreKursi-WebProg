4be4d18559e070059bd030e96aeee426a3f18301
Kevin Halili <kevin.halili@ids.al>
2023-01-31 00:21:07 +0100

    hotels

--- front-end/hotels.php
+++ front-end/hotels.php
@@ -1,19 +1,84 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Hotele dhe Akomodimi</title>
+    <meta charset="UTF-8">
+    <title>Hotele dhe Akomodimi</title>
     <link rel="icon" type="image/x-icon" href="../images/icon.jpg">
-	<script src="../scripts/components.js"></script>
-	<script src="../scripts/scripts.js"></script>
-	<link rel="stylesheet" href="../styles/styles.css">
-	<script src="https://kit.fontawesome.com/1e579789f0.js" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="../scripts/components.js"></script>
+    <script src="../scripts/scripts.js"></script>
+    <link rel="stylesheet" href="../styles/styles.css">
+    <script src="https://kit.fontawesome.com/1e579789f0.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-<body onload="realtimeClock(),getRouting()">
+<body class="hotels" onload="realtimeClock(),getRouting()">
 <?php include "../components/navbar.php" ?>
 <main>
-	<h1>Hotele dhe Akomodimi</h1>
+    <h1>Hotele dhe Akomodimi</h1>
+
+    <div class="reg-container-main">
+        <div class="row">
+            <div class="row">
+                <div class="row-inside">
+                    <h2 class="hotel-name"><em>Best Western Premier Ark Hotel</em></h2>
+                </div>
+                <div class="row-inside">
+                    <div class="column-inside">
+                        <img class="hotel-img" src="../images/hotels/hotel1.jpg" alt="Imazhi nuk u gjet">
+                    </div>
+                    <div class="column-inside">
+                        <h3>Pershkrimi</h3>
+                        <p>Hoteli Best Western Premier Ark ndodhet përballë aeroportit të Tiranës dhe ofron akomodim me
+                            ajër
+                            të kondicionuar me WiFi falas.</p>
+                    </div>
+                    <div class="column-inside">
+                        <h3>Distanca</h3>
+                        <p>50 m nga Aeroporti Ndërkombëtar i Tiranës Nënë Tereza</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="row-inside"><h2 class="hotel-name"><em>Hotel Verzaci</em></h2></div>
+                <div class="row-inside">
+                    <div class="column-inside">
+                        <img class="hotel-img" src="../images/hotels/hotel2.jpg" alt="Imazhi nuk u gjet">
+                    </div>
+                    <div class="column-inside">
+                        <h3>Pershkrimi</h3>
+                        <p>Me një restorant dhe një bar, Hotel Verzaci ndodhet në Aeroportin e Tiranës Nënë Tereza.
+                            Ofron strehim me ajër të kondicionuar me TV me ekran të sheshtë dhe parkim falas në
+                            vend.</p>
+                    </div>
+                    <div class="column-inside">
+                        <h3>Distanca</h3>
+                        <p>450 m from Tirana International Airport Mother Teresa</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="row-inside"><h2 class="hotel-name"><em>Airport Garden Hotel</em></h2></div>
+                <div class="row-inside">
+                    <div class="column-inside">
+                        <img class="hotel-img" src="../images/hotels/hotel3.jpg" alt="Imazhi nuk u gjet">
+                    </div>
+                    <div class="column-inside">
+                        <h3>Pershkrimi</h3>
+                        <p>I ndodhur në Rinas, 18 km nga Sheshi Skënderbej, Airport Garden Hotel ka akomodim me
+                            kopsht,
+                            parkim privat falas, restorant dhe bar. Ky hotel me 3 yje ofron ruajtjen e
+                            bagazheve...</p>
+                    </div>
+                    <div class="column-inside">
+                        <h3>Distanca</h3>
+                        <p>100 m nga Aeroporti Ndërkombëtar i Tiranës Nënë Tereza</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div
+    </div>
+
+
 </main>
 <?php include "../components/footer-bar.php" ?>
 </body>
