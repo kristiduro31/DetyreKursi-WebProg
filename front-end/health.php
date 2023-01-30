bf2086605029ba7e08100287124a5142ef9c6a36
Kevin Halili <kevin.halili@ids.al>
2023-01-30 01:06:49 +0100

    qendra shendetespre

--- front-end/health.php
+++ front-end/health.php
@@ -10,10 +10,22 @@
 	<script src="https://kit.fontawesome.com/1e579789f0.js" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-<body onload="realtimeClock(),getRouting()">
+<body class="health" onload="realtimeClock(),getRouting()">
 <?php include "../components/navbar.php" ?>
 <main>
 	<h1>Qendra Shendetesore</h1>
+    <br>
+        <p>Ambiente të pajisura mirë të ndihmës së parë janë të disponueshme në terminalin tonë. Nëse keni nevojë për ndihmë mjekësore në aeroport, ju lutemi telefononi në numrat e mëposhtëm. Një mjek dhe një
+            ekip i shkëlqyer i Ndihmës së Parë do të jenë aty për ju.
+            Njësia e Ndihmës së Parë <br>
+
+            <em>Telefon: +355696030480</em><br>
+
+            <em>Telefoni: +35544539754</em><br>
+
+            <em>Email: firstaid@tirana-airport.com</em>
+</p>
+
 </main>
 <?php include "../components/footer-bar.php" ?>
 </body>
