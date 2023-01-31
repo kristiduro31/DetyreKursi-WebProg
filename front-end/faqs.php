6af05743d0b344d1d265b001162d5008635df06d
kristiduro <kristi.duro@fti.edu.al>
2023-01-31 13:58:01 +0100

    FAQS finished

--- front-end/faqs.php
+++ front-end/faqs.php
@@ -13,7 +13,37 @@
 <body onload="realtimeClock(),getRouting()">
 <?php include "../components/navbar.php" ?>
 <main>
-	<h1>Pyetje te shpeshta</h1>
+    <div class="reg-container-main">
+        <div class="table-title" style="margin-top: 0px">
+            <h1>Pyetje të shpeshta</h1>
+        </div>
+        <div class="table-title" style="margin-top: 0px">
+            <h3>Cfarë duhet të bëj kur fluturimi im vonohet ose anulohet?</h3>
+        </div>
+        <div class="main-paragraph">
+            <p>Në rast të vonesave ose anulimeve të rënda, ju keni të drejta të caktuara të pasagjerëve dhe mund të keni të drejtë për kompensim ose rimbursim. Autoriteti i Aviacionit Civil (AAC) vendos rregulla të qarta për shumat e paguara në varësi të llojit dhe/ose kohëzgjatjes së vonesës.
+                Linjat ajrore gjithashtu duhet t'ju ofrojnë dokumentacion ose lidhje në mënyrë që të mund të bëni kërkesën tuaj.</p>
+        </div>
+        <div class="table-title" style="margin-top: 0px">
+            <h3>Cfarë duhet të bëj nëse çantat e mia humbasin?</h3>
+        </div>
+        <div class="main-paragraph">
+            <p>Ju keni të drejtë të kërkoni kompensim nga linja ajrore nëse bagazhi juaj i kontrollit vonohet, humbet ose dëmtohet.</p>
+            <p>Ju duhet ta raportoni problemin në linjën ajrore menjëherë. Linjat ajrore ose agjenti i tyre i trajtimit do të kenë përfaqësues në sallën e kthimit të bagazheve. Ju lutemi jepini atyre sa më shumë detaje të jetë e mundur si p.sh. madhësia, forma, ngjyra dhe marka.</p>
+            <p>Ju gjithashtu do të duhet të jepni detajet e kontaktit dhe një adresë dërgimi. Nëse po vizitoni destinacione të shumta, jepni të gjitha adresat pasi mund të duhen disa ditë që çanta juaj të gjendet.
+                Nëse nuk jeni në gjendje të regjistroni të dhënat tuaja në aeroport, keni shtatë ditë për të regjistruar problemin në linjën tuaj ajrore.</p>
+        </div>
+        <div class="table-title" style="margin-top: 0px">
+            <h3>Cilat janë kërkesat për Covid 19 për udhëtime?</h3>
+        </div>
+        <div class="main-paragraph" style="margin-bottom: 35px">
+            <p>Përpara se të udhëtoni, ju lutemi kontrolloni Këshillat e Qeverisë për Udhëtimet e Jashtme për të kuptuar
+                kërkesat për destinacionin tuaj pasi mund të ketë kufizime. Shikoni informacionin tonë për testimin për COVID-19
+                nëse keni nevojë të rezervoni ndonjë test para nisjes për vendin ku po udhëtoni.</p>
+            <p>Kur mbërrini në Shqiperi, aktualisht nuk keni nevojë të bëni ndonjë test para nisjes ose pas mbërritjes për COVID-19. Kjo vlen pavarësisht nëse jeni i vaksinuar apo jo.
+                Për informacionet më të fundit, ju lutemi vizitoni faqet udhëzuese të qeverisë për udhëtimin për COVID.</p>
+        </div>
+    </div>
 </main>
 <?php include "../components/footer-bar.php" ?>
 </body>
